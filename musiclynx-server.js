bd254d8fbf3a40fa9636d1b43a449781a9cdab57
alo <alo@anrita.local>
2017-09-19 17:52:38 +0100

    exclude database reliant modules

--- musiclynx-server.js
+++ musiclynx-server.js
@@ -6,19 +6,22 @@ var bodyParser = require('body-parser');
 var app = module.exports = express();
 
 var ip = process.env.MUSICLYNX_SERVER_PORT || 7757;
+var db_active = false;
 
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/musiclynx', require('./api/musiclynx'));
+if (db_active) {
+  app.use('/musiclynx', require('./api/musiclynx'));
+  app.use('/acousticbrainz', require('./api/acousticbrainz'));
+  app.use('/moodplay', require('./api/moodplay'));
+}
 app.use('/musicbrainz', require('./api/musicbrainz'));
 app.use('/dbpedia', require('./api/dbpedia'));
 app.use('/wikidata', require('./api/wikidata'));
 app.use('/sameas', require('./api/sameas'));
-app.use('/acousticbrainz', require('./api/acousticbrainz'));
-app.use('/moodplay', require('./api/moodplay'));
 app.use('/youtube', require('./api/youtube'));
 app.use('/lastfm', require('./api/lastfm'));
 
