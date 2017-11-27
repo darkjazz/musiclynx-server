caecd707a18cbd21c94acea5751b254c283e4d9b
alo <alo@anrita.local>
2017-11-27 23:44:41 +0000

    updated graph module

--- api/modules/graph.js
+++ api/modules/graph.js
@@ -1,12 +1,12 @@
 var express = require('express');
 
 module.exports.make_graph = function(artists, categories, name, min_ranking) {
-  var graph = { "nodes": [ { "id": name, "group": 0 } ], "links": [] };
+  var graph = { "nodes": [ { "id": name, "group": 1 } ], "links": [] };
   // var categories = {};
   artists.forEach(function(artist) {
     if (artist.ranking >= min_ranking) {
-      graph["nodes"].push({ "id": artist.name, "group": 0 });
-      graph["links"].push({ "source": name, "target": artist.name, "value": artist.ranking });
+      graph["nodes"].push({ "id": artist.name, "group": parseInt(artist.ranking) });
+      graph["links"].push({ "source": name, "target": artist.name, "value": parseInt(artist.ranking) });
     }
   });
   Object.keys(categories).forEach(function(category) {
