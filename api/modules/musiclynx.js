6d62c10a42748edbf5515413bf963aabeb340752
alo <alo@anrita.local>
2018-02-26 14:24:16 +0000

    temporary fix for missing special character artists

--- api/modules/musiclynx.js
+++ api/modules/musiclynx.js
@@ -1,5 +1,6 @@
 var jsonfile = require('jsonfile');
 var uris = require('./uris').uris
+var sa = require('./sameas');
 
 var data = {}
 
@@ -9,16 +10,25 @@ jsonfile.readFile(uris.static_db_dir + uris.linked_static_db + '.json', function
   console.log("Linked artists static data loaded!");
 });
 
-module.exports.find_dbpedia_link = function(mbid, cb) {
+module.exports.find_dbpedia_link = function(mbid, name, cb) {
   if (mbid in data) {
     cb(data[mbid]);
   }
-  else cb({ "status": "artist not found!" });
+  else {
+    // temporary solution
+    sa.find_dbpedia_link(mbid, name, function(dbp_uri) {
+      cb(dbp_uri)
+    })
+  }
 }
 
-module.exports.find_musicbrainz_id = function(artist_uri, cb) {
+module.exports.find_musicbrainz_id = function(artist_uri, name, cb) {
   if (artist_uri in data) {
     cb({'id': data[artist_uri]})
   }
-  else cb({ "status": "artist not found!" })
+  else {
+    sa.find_musicbrainz_id(artist_uri, name, function(artist) {
+      cb(artist)
+    })
+  }
 }
